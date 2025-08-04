afa00d54eb3fac27e010d6b4b1a068252a669b1e
Maximilian Mandlez <maximilian.mandlez@tugraz.at>
2025-08-04 08:26:24 +0200

    refactor: simplify Docker utilities, remove service-specific functions

--- src/ctutor_backend/utils/__init__.py
+++ src/ctutor_backend/utils/__init__.py
@@ -10,8 +10,7 @@ from .color_validation import (
 
 from .docker_utils import (
     is_running_in_docker, get_docker_host_ip, transform_localhost_url,
-    transform_gitlab_url, get_service_url, get_gitlab_url, get_temporal_url,
-    get_minio_url, get_keycloak_url, get_postgres_host
+    transform_gitlab_url
 )
 
 __all__ = [
@@ -21,6 +20,5 @@ __all__ = [
     'get_color_examples',
     # Docker utilities
     'is_running_in_docker', 'get_docker_host_ip', 'transform_localhost_url',
-    'transform_gitlab_url', 'get_service_url', 'get_gitlab_url', 'get_temporal_url',
-    'get_minio_url', 'get_keycloak_url', 'get_postgres_host'
+    'transform_gitlab_url'
 ]
